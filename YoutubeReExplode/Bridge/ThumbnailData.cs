4898b6085facc34daa4582ed7c11cbf046a95f83
Cédric Goffin <goffin.cedric@hotmail.com>
2023-08-06 12:28:14 +0200

    Rebase forked local changes to master

---
+++ YoutubeReExplode/Bridge/ThumbnailData.cs
@@ -0,0 +1,17 @@
+﻿using System.Text.Json;
+using Lazy;
+using YoutubeReExplode.Utils.Extensions;
+
+namespace YoutubeReExplode.Bridge;
+
+internal class ThumbnailData(JsonElement content)
+{
+    [Lazy]
+    public string? Url => content.GetPropertyOrNull("url")?.GetStringOrNull();
+
+    [Lazy]
+    public int? Width => content.GetPropertyOrNull("width")?.GetInt32OrNull();
+
+    [Lazy]
+    public int? Height => content.GetPropertyOrNull("height")?.GetInt32OrNull();
+}
---
+++ YoutubeReExplode/Exceptions/VideoUnavailableException.cs
@@ -0,0 +1,6 @@
+namespace YoutubeReExplode.Exceptions;
+
+/// <summary>
+/// Exception thrown when the requested video is unavailable.
+/// </summary>
+public class VideoUnavailableException(string message) : VideoUnplayableException(message);
