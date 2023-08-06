a91a5bea751f19cb2bd2a2c3c8a34ffd59792d02
Cédric Goffin <goffin.cedric@hotmail.com>
2023-08-06 12:28:14 +0200

    Rebase forked local changes to master

---
+++ YoutubeReExplode.Tests/SearchSpecs.cs
@@ -0,0 +1,76 @@
+using System.Threading.Tasks;
+using FluentAssertions;
+using Xunit;
+using YoutubeReExplode.Common;
+
+namespace YoutubeReExplode.Tests;
+
+public class SearchSpecs
+{
+    [Fact]
+    public async Task I_can_get_results_from_a_search_query()
+    {
+        // Arrange
+        var youtube = new YoutubeClient();
+
+        // Act
+        var results = await youtube.Search.GetResultsAsync("undead corporation");
+
+        // Assert
+        results.Should().HaveCountGreaterOrEqualTo(50);
+    }
+
+    [Fact]
+    public async Task I_can_get_video_results_from_a_search_query()
+    {
+        // Arrange
+        var youtube = new YoutubeClient();
+
+        // Act
+        var videos = await youtube.Search.GetVideosAsync("undead corporation");
+
+        // Assert
+        videos.Should().HaveCountGreaterOrEqualTo(50);
+    }
+
+    [Fact]
+    public async Task I_can_get_live_video_results_from_a_search_query()
+    {
+        // Arrange
+        var youtube = new YoutubeClient();
+
+        // Act
+        var videos = await youtube.Search.GetVideosAsync("lofi");
+
+        // Assert
+        videos.Should().HaveCountGreaterOrEqualTo(1);
+        videos.Should().Contain(searchResult => searchResult.IsLive);
+        videos.Should().NotContain(searchResult => searchResult.IsLiveContent.HasValue);
+    }
+
+    [Fact]
+    public async Task I_can_get_playlist_results_from_a_search_query()
+    {
+        // Arrange
+        var youtube = new YoutubeClient();
+
+        // Act
+        var playlists = await youtube.Search.GetPlaylistsAsync("undead corporation");
+
+        // Assert
+        playlists.Should().NotBeEmpty();
+    }
+
+    [Fact]
+    public async Task I_can_get_channel_results_from_a_search_query()
+    {
+        // Arrange
+        var youtube = new YoutubeClient();
+
+        // Act
+        var channels = await youtube.Search.GetChannelsAsync("undead corporation");
+
+        // Assert
+        channels.Should().NotBeEmpty();
+    }
+}
---
+++ YoutubeReExplode/Utils/ClientDelegatingHandler.cs
@@ -0,0 +1,44 @@
+﻿using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+using YoutubeReExplode.Utils.Extensions;
+
+namespace YoutubeReExplode.Utils;
+
+// Like DelegatingHandler, but wraps an HttpClient instead of an HttpMessageHandler.
+// Used to extend an externally provided HttpClient with additional behavior.
+internal abstract class ClientDelegatingHandler : HttpMessageHandler
+{
+    private readonly HttpClient _http;
+    private readonly bool _disposeClient;
+
+    protected ClientDelegatingHandler(HttpClient http, bool disposeClient = false)
+    {
+        _http = http;
+        _disposeClient = disposeClient;
+    }
+
+    protected override async Task<HttpResponseMessage> SendAsync(
+        HttpRequestMessage request,
+        CancellationToken cancellationToken
+    )
+    {
+        // Clone the request to reset its completion status, which is required
+        // in order to pass the request from one HttpClient to another.
+        using var clonedRequest = request.Clone();
+
+        return await _http.SendAsync(
+            clonedRequest,
+            HttpCompletionOption.ResponseHeadersRead,
+            cancellationToken
+        );
+    }
+
+    protected override void Dispose(bool disposing)
+    {
+        if (disposing && _disposeClient)
+            _http.Dispose();
+
+        base.Dispose(disposing);
+    }
+}
