e4a3975c2f2fe2e3fde4a845baa22e4f63be3bf5
Cédric Goffin <goffin.cedric@hotmail.com>
2023-08-06 12:28:14 +0200

    Rebase forked local changes to master

---
+++ YoutubeReExplode.Tests/SearchSpecs.cs
@@ -0,0 +1,89 @@
+using System.Threading.Tasks;
+using FluentAssertions;
+using Xunit;
+using YoutubeReExplode.Common;
+
+namespace YoutubeReExplode.Tests;
+
+public class SearchSpecs
+{
+    [Fact]
+    public async Task I_can_get_results_from_a_search_query()
+    {
+        // Arrange
+        var youtube = new YoutubeClient();
+
+        // Act
+        var results = await youtube.Search.GetResultsAsync("undead corporation");
+
+        // Assert
+        results.Should().HaveCountGreaterOrEqualTo(50);
+    }
+
+    [Fact]
+    public async Task I_can_get_results_from_a_search_query_that_contains_special_characters()
+    {
+        // Arrange
+        var youtube = new YoutubeClient();
+
+        // Act
+        var results = await youtube.Search.GetResultsAsync("\"dune 2\" ending");
+
+        // Assert
+        results.Should().HaveCountGreaterOrEqualTo(50);
+    }
+
+    [Fact]
+    public async Task I_can_get_video_results_from_a_search_query()
+    {
+        // Arrange
+        var youtube = new YoutubeClient();
+
+        // Act
+        var videos = await youtube.Search.GetVideosAsync("undead corporation");
+
+        // Assert
+        videos.Should().HaveCountGreaterOrEqualTo(50);
+    }
+
+    [Fact]
+    public async Task I_can_get_live_video_results_from_a_search_query()
+    {
+        // Arrange
+        var youtube = new YoutubeClient();
+
+        // Act
+        var videos = await youtube.Search.GetVideosAsync("lofi");
+
+        // Assert
+        videos.Should().HaveCountGreaterOrEqualTo(1);
+        videos.Should().Contain(searchResult => searchResult.IsLive);
+        videos.Should().NotContain(searchResult => searchResult.IsLiveContent.HasValue);
+    }
+
+    [Fact]
+    public async Task I_can_get_playlist_results_from_a_search_query()
+    {
+        // Arrange
+        var youtube = new YoutubeClient();
+
+        // Act
+        var playlists = await youtube.Search.GetPlaylistsAsync("undead corporation");
+
+        // Assert
+        playlists.Should().NotBeEmpty();
+    }
+
+    [Fact]
+    public async Task I_can_get_channel_results_from_a_search_query()
+    {
+        // Arrange
+        var youtube = new YoutubeClient();
+
+        // Act
+        var channels = await youtube.Search.GetChannelsAsync("undead corporation");
+
+        // Assert
+        channels.Should().NotBeEmpty();
+    }
+}
