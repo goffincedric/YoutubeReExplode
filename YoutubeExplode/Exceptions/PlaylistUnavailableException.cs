7e00da0b9f6ce4acaee960688b4dfddfb2cd196d
Cédric Goffin <goffin.cedric@hotmail.com>
2023-08-06 12:28:14 +0200

    Rebase forked local changes to master

--- YoutubeExplode/Exceptions/PlaylistUnavailableException.cs
+++
@@ -1,6 +0,0 @@
-namespace YoutubeExplode.Exceptions;
-
-/// <summary>
-/// Exception thrown when the requested playlist is unavailable.
-/// </summary>
-public class PlaylistUnavailableException(string message) : YoutubeExplodeException(message);
