c901c1ee5bf0a9f9692e6f261953270605a8a635
Cédric Goffin <goffin.cedric@hotmail.com>
2024-10-02 21:30:31 +0200

    Rebase changes onto master

---
+++ YoutubeReExplode/Videos/Music.cs
@@ -0,0 +1,32 @@
+namespace YoutubeReExplode.Videos;
+
+/// <summary>
+/// Music details of video.
+/// </summary>
+public class Music
+{
+    /// <summary>
+    /// Song name.
+    /// </summary>
+    public string? Song { get; }
+
+    /// <summary>
+    /// Name of artists.
+    /// </summary>
+    public string[]? Artists { get; }
+
+    /// <summary>
+    /// Album name.
+    /// </summary>
+    public string? Album { get; }
+
+    /// <summary>
+    /// Initializes an instance of <see cref="Music" />.
+    /// </summary>
+    public Music(string? song, string[]? artists, string? album)
+    {
+        Song = song;
+        Artists = artists;
+        Album = album;
+    }
+}
