bd5a414830f65971f39fe8556544e0ee93bbe05e
Cédric Goffin <goffin.cedric@hotmail.com>
2024-10-02 21:30:31 +0200

    Rebase changes onto master

--- YoutubeExplode/Exceptions/RequestLimitExceededException.cs
+++
@@ -1,6 +0,0 @@
-namespace YoutubeExplode.Exceptions;
-
-/// <summary>
-/// Exception thrown when YouTube denies a request because the client has exceeded rate limit.
-/// </summary>
-public class RequestLimitExceededException(string message) : YoutubeExplodeException(message);
